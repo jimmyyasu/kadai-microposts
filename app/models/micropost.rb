e11107cc8b1c4ea5ec0da569e7d91f2e079881c0
yasuhiro <ec2-user@ip-172-31-88-147.ec2.internal>
2020-05-23 03:26:50 +0000

    follow, unfollow

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,15 @@ class Micropost < ApplicationRecord
   
   validates :content, presence: true, length: { maximum: 255 }
   
+  has_many :favorites
+  has_many :users, through: :favorites
+  
+  def favorite(user)
+    favorites.create(user_id: user.id)
+  end
+  
+  def unfavorite(user)
+    favorites.find_by(user_id: user.id).destroy
+  end
+  
 end
