8a6e218ad13bfce8011e08a994dab1a0ef9a60fe
yasuhiro <ec2-user@ip-172-31-88-147.ec2.internal>
2020-06-05 06:34:56 +0000

    micropost-favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,17 @@
 class FavoritesController < ApplicationController
-  before_action :require_user_logged_in
+before_action :require_user_logged_in
   
   def create
-    user = User.find(params[:user_id])
+    micropost = Micropost.find(params[:favorite_id])
     current_user.favorite(micropost)
-    flash[:success] = 'お気に入りしました。'
-    redirect_to user
+    flash[:success] = 'お気に入りに登録しました。'
+    redirect_to root_url
   end
 
   def destroy
-    user = User.find(params[:user_id])
+    micropost= Micropost.find(params[:favorite_id])
     current_user.unfavorite(micropost)
     flash[:success] = 'お気に入りを解除しました。'
-    redirect_to user
+    redirect_to root_url
   end
 end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,17 +1,10 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites
+  
   validates :content, presence: true, length: { maximum: 255 }
   
-  has_many :favorites
-  has_many :users, through: :favorites
-  
-  def favorite(user)
-    favorites.create(user_id: user.id)
-  end
-  
-  def unfavorite(user)
-    favorites.find_by(user_id: user.id).destroy
-  end
-  
-end
+    
+end 
